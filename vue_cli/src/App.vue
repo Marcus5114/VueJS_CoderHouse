a75419df0dd1a252c29f1a1399fbe1efe6df6e15
Marcus5114 <fentraigas@yahoo.com.ar>
2023-06-13 18:34:20 -0300

    vue cli

---
+++ vue_cli/src/App.vue
@@ -0,0 +1,78 @@
+<template>
+ <div class="container-fluid">
+            <div class="container w-75">
+
+                <h1 class="text-center">Componentes</h1>
+
+                <hr>
+                <br>
+
+                <div class="row row-cols-3">
+                <UserCard v-for="usuarios in infoUsuarios" :key="usuarios.id"
+                            :nombre ="usuarios.name"
+                            :correo ="usuarios.email"
+                            :ciudad ="usuarios.city"
+                            :pais ="usuarios.country"
+                            :depto ="usuarios.department"
+                            :compania ="usuarios.company"
+                            :antiguedad ="usuarios.seniority"
+                ></UserCard>
+                </div>
+
+                
+
+            </div>
+        </div>
+</template>
+
+<script>
+import UserCard from './components/UserCard.vue'
+
+
+
+export default {
+  name: 'App',
+  data() {
+    return {
+
+      infoUsuarios: [
+        {
+          "name": "Donna Griffith",
+          "email": "nunc.mauris@protonmail.couk",
+          "city": "Fort Wayne",
+          "country": "United States",
+          "department": "Customer Service",
+          "company": "Yahoo",
+          "seniority": "5"
+        },
+        {
+          "name": "Yasir Curtis",
+          "email": "phasellus.elit.pede@outlook.org",
+          "city": "Fayetteville",
+          "country": "United States",
+          "department": "Customer Service",
+          "company": "Google",
+          "seniority": "12"
+        },
+        {
+          "name": "Tashya Henry",
+          "email": "neque.pellentesque@outlook.couk",
+          "city": "Topeka",
+          "country": "United States",
+          "department": "Customer Service",
+          "company": "Apple",
+          "seniority": "13"
+        }
+      ]
+    }
+  },
+  components: {
+    UserCard
+  }
+}
+</script>
+
+<style>
+
+
+</style>
---
+++ vue_cli/src/components/UserCard.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="col"> 
+    <div class="card" :class="{temaOscuro: cardTheme}">
+      <div class="card-header fw-bold">
+        👤 User Data
+      </div>
+      <div class="card-body">
+        <h5 class="card-title">{{ nombre }}</h5>
+        <small class="text-muted"> 🌐 {{ ciudad }} / {{ pais }}</small>
+
+        <hr>
+
+        <div class="table-responsive">
+
+          <table class="table table-striped">
+            <tbody>
+              <tr>
+                <th> 🏢 Comapany</th>
+                <td>{{ compania }}</td>
+  
+              </tr>
+
+              <tr>
+                <th> 🗄 Department</th>
+                <td>{{ depto }}</td>
+  
+              </tr>
+
+              <tr>
+                <th> 📅 Seniority</th>
+                <td>{{ antiguedad }} Years</td>
+  
+              </tr>
+            </tbody>
+          </table>
+
+        </div>
+
+        <div class="text-end">
+          <small class="text-muted">Change card theme</small>
+          <br>
+
+          <button type="button" class="btn btn-light btn-sm" @click="cardTheme = !cardTheme"><span  v-if="cardTheme">Light</span><span  v-else>Dark</span> </button>
+
+        </div>
+
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserCard',
+  data() {
+    return {
+
+      cardTheme: false
+
+    }
+  },
+  props: {
+    nombre: String,
+    correo: String,
+    ciudad: String,
+    pais: String,
+    depto: String,
+    compania: String,
+    antiguedad: String
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.temaOscuro * {
+      color: #fff !important;
+      background-color: #212529;
+  }
+
+
+</style>
